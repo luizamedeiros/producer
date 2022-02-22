c5b8a340d5a7f75f4e186760a678934f5ebfac9e
Luiza Medeiros <luizadmeds@gmail.com>
2022-02-22 19:19:21 -0300

    Producer up and running

--- src/main/java/com/example/producer/ProducerApplication.java
+++ src/main/java/com/example/producer/ProducerApplication.java
@@ -1,13 +1,27 @@
 package com.example.producer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication implements CommandLineRunner{
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		for (int i = 0; i<20; i++) {
+			rabbitTemplate.convertAndSend("testQueue", "Hello: " + i + 1);
+		}
 	}
 
 }
